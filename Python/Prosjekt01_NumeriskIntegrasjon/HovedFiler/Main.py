adcd250c67177f0acf7dff6e64f7aa939b97570e
Lisan <lisankuruppu@gmail.com>
2023-03-23 15:34:50 +0100

    sdf

--- Python/Prosjekt01_NumeriskIntegrasjon/HovedFiler/Main.py
+++ Python/Prosjekt01_NumeriskIntegrasjon/HovedFiler/Main.py
@@ -43,8 +43,8 @@ timer = clock()				# timerobjekt med tic toc funksjoner
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                            1) KONFIGURASJON
 #
-Configs.EV3_IP = "169.254.119.149"	# Avles IP-adressen på EV3-skjermen
-Configs.Online = False	# Online = True  --> programmet kjører på robot  
+Configs.EV3_IP = "169.254.189.67"	# Avles IP-adressen på EV3-skjermen
+Configs.Online = True	# Online = True  --> programmet kjører på robot  
 						# Online = False --> programmet kjører på datamaskin
 Configs.livePlot = False 	# livePlot = True  --> Live plot, typisk stor Ts
 							# livePlot = False --> Ingen plot, liten Ts
