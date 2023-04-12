961835e954cc10f313b21548a77ea27d2b2518dc
Lisan <lisankuruppu@gmail.com>
2023-04-12 16:41:05 +0200

    asdfasdfasfasfasd

--- Python/Prosjekt01_NumeriskIntegrasjon/HovedFiler/Main.py
+++ Python/Prosjekt01_NumeriskIntegrasjon/HovedFiler/Main.py
@@ -219,19 +219,19 @@ def MathCalculations(data,k,init):
 
 	# Parametre
 	a = 0.7
-
-	# Tilordne målinger til variable
-	data.Flow.append(data.Lys[k] - data.Lys[0]) 
+	init.nullflow = data.Lys[0]
 
 		# Initialverdier og beregninger 
 	if k == 0:
 		# Initialverdier
 		data.Ts.append(0.005)  	# nominell verdi
+		data.Flow.append(init.nullflow)
 		data.Volum.append(0)
 		
 	else:
 		# Beregninger av Ts og variable som avhenger av initialverdi
 		data.Ts.append(data.Tid[k]-data.Tid[k-1])
+		data.Flow.append(data.Lys[k] - init.nullflow)
 		data.Volum.append(data.Volum[-1] + data.Flow[-1] * data.Ts[-1])
 
 	# Andre beregninger uavhengig av initialverdi
