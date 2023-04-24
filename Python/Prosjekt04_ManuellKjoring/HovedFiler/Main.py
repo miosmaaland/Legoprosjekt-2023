fcef7729646af078d2a2c1adf29a25e0084be8f6
eple1606 <r.hoang1606@hotmail.com>
2023-04-24 17:14:15 +0200

    ædsgkgkl

--- Python/Prosjekt04_ManuellKjoring/HovedFiler/Main.py
+++ Python/Prosjekt04_ManuellKjoring/HovedFiler/Main.py
@@ -43,30 +43,18 @@ timer = clock()				# timerobjekt med tic toc funksjoner
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                            1) KONFIGURASJON
 #
-<<<<<<< HEAD
-Configs.EV3_IP = "169.254.216.218"	# Avles IP-adressen på EV3-skjermen
-Configs.Online = False	# Online = True  --> programmet kjører på robot  
-=======
 Configs.EV3_IP = "169.254.197.50"	# Avles IP-adressen på EV3-skjermen
-Configs.Online = True	# Online = True  --> programmet kjører på robot  
->>>>>>> 4bef09b5fb55d6490a684be0d7fe9b7170ad317e
+Configs.Online = False	# Online = True  --> programmet kjører på robot  
 						# Online = False --> programmet kjører på datamaskin
 Configs.livePlot = False 	# livePlot = True  --> Live plot, typisk stor Ts
 							# livePlot = False --> Ingen plot, liten Ts
 Configs.avgTs = 0.005	# livePlot = False --> spesifiser ønsket Ts
 						# Lav avgTs -> høy samplingsfrekvens og mye data.
 						# --> Du må vente veldig lenge for å lagre filen.
-<<<<<<< HEAD
 Configs.filename = "P04_ManuellKjøring_Rich.txt"	
 						# Målinger/beregninger i Online lagres til denne 
 						# .txt-filen. Upload til Data-mappen.
 Configs.filenameOffline = "Offline_P04_ManuellKjøring_Rich.txt"	
-=======
-Configs.filename = "P04_ManuellKjøring_Mio.txt"	
-						# Målinger/beregninger i Online lagres til denne 
-						# .txt-filen. Upload til Data-mappen.
-Configs.filenameOffline = "Offline_P04_ManuellKjøring_Mio.txt"	
->>>>>>> 4bef09b5fb55d6490a684be0d7fe9b7170ad317e
 						# I Offline brukes den opplastede datafilen 
 						# og alt lagres til denne .txt-filen.
 Configs.plotMethod = 2	# verdier: 1 eller 2, hvor hver plottemetode 
