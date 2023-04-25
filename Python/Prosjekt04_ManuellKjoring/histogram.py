5ca50df20be6b7f5f5014ffa9a2c6f8c97cbcb9c
Lisan <lisankuruppu@gmail.com>
2023-04-25 21:06:47 +0200

    aølksdfjslkaøjfslkdajf

--- Python/Prosjekt04_ManuellKjoring/histogram.py
+++ Python/Prosjekt04_ManuellKjoring/histogram.py
@@ -13,6 +13,7 @@ lines = data.split('\n')
 
 # Extract the values of Lys into a list
 Lys_L = []
+Ts_L = []
 for line in lines:
     # Skip any empty lines
     if line:
@@ -22,8 +23,9 @@ for line in lines:
         if len(values) >= 9:
             # Extract the value of Lys from the list
             Lys = float(values[8])
-            # Append the value of Lys to the list of Lys values
             Lys_L.append(Lys)
+            Ts = float(values[4])
+            Ts_L .append(Ts)
 
 # Open the file for reading
 with open('Data\Offline_P04_ManuellKjøring_Mio.txt', 'r') as f:
@@ -37,6 +39,7 @@ lines = data.split('\n')
 
 # Extract the values of Lys into a list
 Lys_M = []
+Ts_M = []
 for line in lines:
     # Skip any empty lines
     if line:
@@ -48,6 +51,8 @@ for line in lines:
             Lys = float(values[8])
             # Append the value of Lys to the list of Lys values
             Lys_M.append(Lys)
+            Ts = float(values[4])
+            Ts_M .append(Ts)
 
 with open('Data\Offline_P04_ManuellKjøring_Rich.txt', 'r') as f:
     # Skip the first line (header)
@@ -60,6 +65,7 @@ lines = data.split('\n')
 
 # Extract the values of Lys into a list
 Lys_R = []
+Ts_R = []
 for line in lines:
     # Skip any empty lines
     if line:
@@ -71,6 +77,8 @@ for line in lines:
             Lys = float(values[8])
             # Append the value of Lys to the list of Lys values
             Lys_R.append(Lys)
+            Ts = float(values[4])
+            Ts_R .append(Ts)
 
 with open('Data\Offline_P04_ManuellKjøring_Sanjai.txt', 'r') as f:
     # Skip the first line (header)
@@ -83,6 +91,7 @@ lines = data.split('\n')
 
 # Extract the values of Lys into a list
 Lys_S = []
+Ts_S = []
 for line in lines:
     # Skip any empty lines
     if line:
@@ -94,34 +103,40 @@ for line in lines:
             Lys = float(values[8])
             # Append the value of Lys to the list of Lys values
             Lys_S.append(Lys)
+            Ts = float(values[4])
+            Ts_S .append(Ts)
 
 # Create a histogram of the Lys values
-import matplotlib.pyplot as plt
+
+print("Mean value of Ts_L:", statistics.mean(Ts_L))
+print("Mean value of Ts_M:", statistics.mean(Ts_M))
+print("Mean value of Ts_R:", statistics.mean(Ts_R))
+print("Mean value of Ts_S:", statistics.mean(Ts_S))
 
 # Plot the histograms
 fig, axs = plt.subplots(2, 2, figsize=(10, 10))
 
 # Lys_L histogram
 axs[0, 0].hist(Lys_L, bins=50, color='blue', alpha=0.7)
-axs[0, 0].set_title(f'Lisan, Mean={statistics.median(Lys_L)}, std={statistics.stdev(Lys_L): .2f}')
+axs[0, 0].set_title(f'Lisan, Mean={statistics.mean(Lys_L): .2f}, std={statistics.stdev(Lys_L): .2f}')
 axs[0, 0].set_xlabel('Lysverdier')
 axs[0, 0].set_ylabel('Antall målinger')
 
 # Lys_M histogram
 axs[0, 1].hist(Lys_M, bins=50, color='red', alpha=0.7)
-axs[0, 1].set_title(f'Mio, Mean={statistics.median(Lys_M)}, std={statistics.stdev(Lys_M): .2f}')
+axs[0, 1].set_title(f'Mio, Mean={statistics.mean(Lys_M): .2f}, std={statistics.stdev(Lys_M): .2f}')
 axs[0, 1].set_xlabel('Lysverdier')
 axs[0, 1].set_ylabel('Antall målinger')
 
 # Lys_R histogram
 axs[1, 0].hist(Lys_R, bins=50, color='green', alpha=0.7)
-axs[1, 0].set_title(f'Rich, Mean={statistics.median(Lys_R)}, std={statistics.stdev(Lys_R): .2f}')
+axs[1, 0].set_title(f'Rich, Mean={statistics.mean(Lys_R): .2f}, std={statistics.stdev(Lys_R): .2f}')
 axs[1, 0].set_xlabel('Lysverdier')
 axs[1, 0].set_ylabel('Antall målinger')
 
 # Lys_R histogram
 axs[1, 1].hist(Lys_S, bins=50, color='yellow', alpha=0.7)
-axs[1, 1].set_title(f'Sanjai, Mean={statistics.median(Lys_S)}, std={statistics.stdev(Lys_S): .2f}')
+axs[1, 1].set_title(f'Sanjai, Mean={statistics.mean(Lys_S): .2f}, std={statistics.stdev(Lys_S): .2f}')
 axs[1, 1].set_xlabel('Lysverdier')
 axs[1, 1].set_ylabel('Antall målinger')
 
