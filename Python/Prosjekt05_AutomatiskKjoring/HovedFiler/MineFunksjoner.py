887b108546f5422e6d4fc364fb14082483f1fa35
miosmaaland <miosmaaland@gmail.com>
2023-04-26 14:26:43 +0200

    baddabim
    baddabim

---
+++ Python/Prosjekt05_AutomatiskKjoring/HovedFiler/MineFunksjoner.py
@@ -0,0 +1,26 @@
+# Her skriver du funksjoner som skal brukes i MathCalculations
+# Etter å ha skrevet dem her kan du kalle på dem i Main.py filen (De blir automatisk importert)
+def EulerForward(IntValue, FunctionValue, TimeStep):
+		IntNewValue = IntValue + (FunctionValue * TimeStep)
+		return IntNewValue
+
+
+def FIR_Filter(Measurements, M):
+	Filtered_FIR = (sum(Measurements) / M)	
+	return Filtered_FIR
+
+
+def IIR_Filter(FilteredValue, Measurement, alfa):
+	# Parameters
+	a = 1 - alfa
+	b = alfa
+	
+	Filtered_IIR = (a * FilteredValue+ b * Measurement)
+		
+	return Filtered_IIR
+
+
+def Derivation(FunctionValues, TimeStep):
+	derivative = (FunctionValues) / TimeStep
+	
+	return derivative
