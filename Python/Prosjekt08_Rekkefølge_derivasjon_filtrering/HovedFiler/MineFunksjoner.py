2123dd4d03d96aa5769bbe619a83db93a7d8bbae
eple1606 <r.hoang1606@hotmail.com>
2023-04-27 18:59:26 +0200

    12

---
+++ Python/Prosjekt08_Rekkefølge_derivasjon_filtrering/HovedFiler/MineFunksjoner.py
@@ -0,0 +1,21 @@
+# Her skriver du funksjoner som skal brukes i MathCalculations
+# Etter å ha skrevet dem her kan du kalle på dem i Main.py filen (De blir automatisk importert)
+def FIR_Filter(Measurements, M):
+	Filtered_FIR = (sum(Measurements) / M)	
+	return Filtered_FIR
+
+
+def IIR_Filter(FilteredValue, Measurement, alfa):
+	# Parameters
+	a = 1 - alfa
+	b = alfa
+	
+	Filtered_IIR = (a * FilteredValue+ b * Measurement)
+		
+	return Filtered_IIR
+
+
+def Derivation(FunctionValues, TimeStep):
+	derivative = (FunctionValues[1] - FunctionValues[0]) / TimeStep
+	
+	return derivative
